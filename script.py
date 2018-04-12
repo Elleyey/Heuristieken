4dbba5fac5eed5f0c3ddb121b2687e9c32d38996
JetteKA <jette@korthalsaltes.comJetteKA>
2018-04-12 13:25:29 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,24 @@
+GROUND_LENGTH = 180 # meter
+GROUND_WIDTH = 160 # meter
+WATER_SURF = 0.2 * GROUND_WIDTH * GROUND_LENGTH # square meter
+
+class Eengezinswoning:
+    house_length = 8 # meter
+    house_width = 8 # meter
+    price = 285 # thousand euro
+    vrijstand = 2 # meter
+    xtra_vrijstand_value = 1.03 # added value to price per extra meter vrijstand
+
+class Bungalow:
+    house_length = 10 # meter
+    house_width = 7.5 # meter
+    price = 399 # thousand euro
+    vrijstand = 3 # meter
+    xtra_vrijstand_value = 1.04 # added value to price per extra meter vrijstand
+
+class Maison:
+    house_length = 11 # meter
+    house_width = 10.5 # meter
+    price = 610 # thousand euro
+    vrijstand = 6 # meter
+    xtra_vrijstand_value = 1.06 # added value to price per exta meter vrijstand
